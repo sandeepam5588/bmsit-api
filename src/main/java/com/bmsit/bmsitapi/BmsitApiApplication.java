7e10ffcabccd82b62d52c7ffa98c7b129ba6676f
sandeepam5588 <sandeepm.mice@gmail.com>
2022-05-16 16:20:59 +0530

    Initial Commit

---
+++ src/main/java/com/bmsit/bmsitapi/BmsitApiApplication.java
@@ -0,0 +1,13 @@
+package com.bmsit.bmsitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsitApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmsit/bmsitapi/BmsitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmsit.bmsitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
